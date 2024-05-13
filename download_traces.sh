c19b1af6fb5ba5953e1f9aa91d5baa86f3a8dd11
Gordei Pribõtkin <pribytkingordei@gmail.com>
2024-05-14 00:28:53 +0300

    change download_traces

--- download_traces.sh
+++ download_traces.sh
@@ -11,15 +11,16 @@ curl -o "$TARGET_DIR/file.zip" "$ZIP_URL"
 unzip "$TARGET_DIR/file.zip" -d "$TARGET_DIR"
 
 # Get the name of the unzipped folder (assuming there's only one folder in the zip)
-UNZIPPED_FOLDER=$(unzip -l "$TARGET_DIR/file.zip" | awk '/^ *[0-9]/ {print $4}' | cut -d/ -f1 | sort | uniq | head -n 1)
+UNZIPPED_FOLDER="vista-public-traces"
 
-cd "$UNZIPPED_FOLDER"
-# Rename the unzipped folder
-mv "$TARGET_DIR/$UNZIPPED_FOLDER/*forward*" "$TARGET_DIR/forward_trace"
-mv "$TARGET_DIR/$UNZIPPED_FOLDER/*backward*" "$TARGET_DIR/backward_trace"
+unzip "$TARGET_DIR/$UNZIPPED_FOLDER/*forward*.zip" -d "$TARGET_DIR"
+unzip "$TARGET_DIR/$UNZIPPED_FOLDER/*backward*.zip" -d "$TARGET_DIR"
+mv $TARGET_DIR/*forward* $TARGET_DIR/forward_trace
+mv $TARGET_DIR/*backward* $TARGET_DIR/backward_trace
 
-# Clean up: remove the zip file
+# Clean up
 rm "$TARGET_DIR/file.zip"
-rm "$TARGET_DIR/$UNZIPPED_FOLDER"
+rm -rf "$TARGET_DIR/$UNZIPPED_FOLDER"
+rm -rf $TARGET_DIR/*ebm-*
 
 echo "Download, unzip, and rename completed successfully."
